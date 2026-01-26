0eca2e01b9c65161ba50f3b1d7127a1991c55b65
Emerson Araújo <emersonsantos0302@gmail.com>
2026-01-25 21:33:04 -0300

    configurando o setup

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compilaSass() {
+    return gulp.src('src/styles/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('dist/css'));
+}
+
+exports.default = compilaSass;
+
+exports.watch = function() {
+    gulp.watch('src/styles/**/*.scss', compilaSass);
+}
