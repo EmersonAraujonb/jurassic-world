c49124747b25ec099fadae1f6a8c8ed4677deaf5
Emerson Araújo <emersonsantos0302@gmail.com>
2026-02-27 13:21:08 -0300

    correção

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,51 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
+import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
+import * as dartSass from 'sass';
+import uglify from 'gulp-uglify';
+import imagemin from 'gulp-imagemin';
+const sass = gulpSass(dartSass);
 
-function otimizaImagens() {
-    return gulp.src('src/images/**/*.{jpg,jpeg,png,gif,svg,webp}')
-        .pipe(imagemin([
-            imagemin.mozjpeg({ quality: 75, progressive: true }),
-            imagemin.optipng({ optimizationLevel: 5 }),
-            imagemin.svgo()
-        ]))
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+export function otimizaImagens() {
+    return gulp.src('src/images/**/*')
+        .pipe(gulp.dest('dist/images'));
 }
 
-function comprimeJavaScript() {
-    return gulp.src('src/scripts/*.js')
+export function comprimeJavaScript() {
+    return gulp.src('src/scripts/**/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/scripts'));
+        .pipe(gulp.dest('dist/scripts'));
 }
 
-function compilaSass() {
+export function compilaSass() {
     return gulp.src('src/styles/**/*.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('dist/css'));
 }
 
-function html() {
-    return gulp.src('./index.html')
-        .pipe(gulp.dest('./dist'))
+export function html() {
+    return gulp.src('index.html')
+        .pipe(gulp.dest('dist'));
 }
 
-function videos() {
+export function videos() {
     return gulp.src('src/videos/**/*')
-        .pipe(gulp.dest('./dist/videos'));
+        .pipe(gulp.dest('dist/videos'));
 }
 
-exports.default = gulp.series(
+export const build = gulp.parallel(
     html,
     videos,
-    gulp.parallel(
-        compilaSass,
-        comprimeJavaScript,
-        otimizaImagens
-    )
+    compilaSass,
+    comprimeJavaScript,
+    otimizaImagens
 );
+
+export function watch() {
+    gulp.watch('src/styles/**/*.scss', compilaSass);
+    gulp.watch('src/images/**/*', otimizaImagens);
+    gulp.watch('src/scripts/**/*.js', comprimeJavaScript);
+    gulp.watch('index.html', html);
+    gulp.watch('src/videos/**/*', videos);
+}
+
+export default gulp.series(build, watch);
